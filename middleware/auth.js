06f8e2f488f41249319946a71640a9d9e7409e17
ValouZ <debray.va@gmail.com>
2021-05-05 10:46:37 +0200

    correction middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,9 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    console.log("MIDDLEWARE");
-    const token = req.headers.authorization;
+    console.log('MIDDLEWARE')
+    const token = req.headers.authorization.split(' ')[1];
+    console.log(token)
     const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
     const userId = decodedToken.userId;
     if (req.body.userId && req.body.userId !== userId) {
